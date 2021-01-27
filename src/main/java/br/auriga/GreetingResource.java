5942fb0a1168a17363ab2abc0fa9c0780af3b57e
Rafael Morelli <kmorelli@gmail.com>
2021-01-26 21:19:26 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/auriga/GreetingResource.java
@@ -0,0 +1,34 @@
+package br.auriga;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello-resteasy")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/teste")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String teste() {
+    	return "Testandoooo";
+    }
+
+    @GET
+    @Path("/teste/{nome}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String teste(@PathParam("nome") String nome) {
+    	return "Voce esta testando por " + nome + " ?";
+    }
+
+}
+
+
